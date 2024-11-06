f94163abb89723027e00b796a20cfedf9ec86250
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2024-11-06 22:19:27 +0100

    Entrega total del Mastemind, no estoy seguro de que esten todos los documentos necesarios subidos

---
+++ PR1SergioVegas.md
@@ -0,0 +1,68 @@
+# Master Mind
+
+## Robust
+El programa s'executa sense problemes i no apareixen missatges d'error (traces) a la consola (tant per mala codificació com per fer depuració). Fa el que es demana (al 100% correcte).
+
+Totes les entrades que li dones a la consola  et dona un missatge de que has fet un error i et deixa tornar a inserir les dades.
+
+![Exemple 1](/Imatges_Mastermind/Ejemplo%201.png)
+
+![Exemple 2](/Imatges_Mastermind/Ejemplo%202.png)
+
+## Claredat i ordre
+L'estructura del programa es pot seguir amb claredat. Les instruccions són clares i fàcils de seguir. Segueix les convencions treballades a classe i de la documentació del Clean Code. El codi conté els comentaris necessaris.
+
+No hi ha cap part del codi que no sigui llegible i les variables i constant deixan molt clar sobre que part estic treballant.
+
+## Variables i constants
+
+Els noms de les variables i constants són prou autodescriptius i segueixen la notació de la comunitat i en anglès. Tots els literals (textos) estan declarats com a constants. Els tipus són correctes. Les variables i constants es declaren a principi del codi.
+
+Totes les constants estan declarades en un mateix lloc, amb nomb clars i intuitius.
+
+![Exemple 3](/Imatges_Mastermind/Ejemplo%203.png)
+
+## Estructures de control
+
+Les condicions a les estructures condicionals i iteratives són correctes, no donen lloc a indefinicions i tots els fluxes del codi retornen un valor. No es fan servir sentències break/continue per finalitzar les condicions/iteracions (excepte en swicth).
+
+![Exemple 4](/Imatges_Mastermind/Ejemplo%204.png)
+
+## Gestió d'Errors
+
+Es controlen correctament els possibles errors (valors introduïts per teclat) així com els que es poden produir en executar el programa.
+
+Al exemple 1 i 2 es pot veure com els errors estan controlats.
+
+## Disseny Modular
+
+S'ha fent un bon ús del disseny modular, aplicant mètodes en casos adients i amb una bona estructura modular. S'ha aplicat la sobrecàrrega de mètodes allà on era possible.
+
+## UX/UI
+
+Els missatges d'interacció amb l'usuari són prou intuïtius i permeten entendre i seguir el joc; estan adaptats a la instrucció a executar. La interacció del joc és user-friendly (amigable).
+
+Al exemple 3 es pot veure tots el missatges.
+## Lliurament
+
+S'ha entregat tot el que es demanava (solució amb .gitignore, executable, READ.me, evidències proves) seguint la nomenclatura indicada. El programa demostra que el seu autor s'ha documentat però el codi és original (no s'ha copiat ni ha fet servir eines IA).
+## Extra 2 - Menú
+
+En finalitzar el joc torna a mostrar el menú inicial i finalitza el joc sense clàusules Exit.
+![Exemple 5]
+
+## Extra 3 - Estètica
+Fa servir decoracions que donen un toc diferenciat i immersiu del programa (agradables per a l'usuari), respectant la UX.
+
+Durant tot el exemples es pot veure l'estetica i la decoració.
+## Joc de proves
+S'entreguen evidències de com a mínim 8 casos de proves diferents.
+
+![Cas 1](/Imatges_Mastermind/Cas%20prova%201.png)
+![Cas 2](/Imatges_Mastermind/Cas%20prova%202.png)
+![Cas 3](/Imatges_Mastermind/Cas%20prova%203.png)
+![Cas 4](/Imatges_Mastermind/Cas%20prova%204.png)
+![Cas 5](/Imatges_Mastermind/Cas%20prova%205.png)
+![Cas 6](/Imatges_Mastermind/Cas%20prova%206.png)
+![Cas 7](/Imatges_Mastermind/Cas%20prova%207.png)
+![Cas 8](/Imatges_Mastermind/Cas%20prova%208.png)
---
+++ Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+        const string CombinacioSecreta = "1 5 3 1";
+        const string MasterMind = " /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\ \r\n( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )\r\n > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ < \r\n /\\_/\\           ___    ,---.        ,-,--.  ,--.--------.    ,----.                 /\\_/\\ \r\n( o.o )   .-._ .'=.'\\ .--.'  \\     ,-.'-  _\\/==/,  -   , -\\,-.--` , \\  .-.,.---.    ( o.o )\r\n > ^ <   /==/ \\|==|  |\\==\\-/\\ \\   /==/_ ,_.'\\==\\.-.  - ,-./==|-  _.-` /==/  `   \\    > ^ < \r\n /\\_/\\   |==|,|  / - |/==/-|_\\ |  \\==\\  \\    `--`\\==\\- \\  |==|   `.-.|==|-, .=., |   /\\_/\\ \r\n( o.o )  |==|  \\/  , |\\==\\,   - \\  \\==\\ -\\        \\==\\_ \\/==/_ ,    /|==|   '='  /  ( o.o )\r\n > ^ <   |==|- ,   _ |/==/ -   ,|  _\\==\\ ,\\       |==|- ||==|    .-' |==|- ,   .'    > ^ < \r\n /\\_/\\   |==| _ /\\   /==/-  /\\ - \\/==/\\/ _ |      |==|, ||==|_  ,`-._|==|_  . ,'.    /\\_/\\ \r\n( o.o )  /==/  / / , |==\\ _.\\=\\.-'\\==\\ - , /      /==/ -//==/ ,     //==/  /\\ ,  )  ( o.o )\r\n > ^ <   `--`./  `--` `--`         `--`---'       `--`--``--`-----`` `--`-`--`--'    > ^ < \r\n /\\_/\\           ___   .=-.-..-._                                                    /\\_/\\ \r\n( o.o )   .-._ .'=.'\\ /==/_ /==/ \\  .-._  _,..---._                                 ( o.o )\r\n > ^ <   /==/ \\|==|  |==|, ||==|, \\/ /, /==/,   -  \\                                 > ^ < \r\n /\\_/\\   |==|,|  / - |==|  ||==|-  \\|  ||==|   _   _\\                                /\\_/\\ \r\n( o.o )  |==|  \\/  , |==|- ||==| ,  | -||==|  .=.   |                               ( o.o )\r\n > ^ <   |==|- ,   _ |==| ,||==| -   _ ||==|,|   | -|                                > ^ < \r\n /\\_/\\   |==| _ /\\   |==|- ||==|  /\\ , ||==|  '='   /                                /\\_/\\ \r\n( o.o )  /==/  / / , /==/. //==/, | |- ||==|-,   _`/                                ( o.o )\r\n > ^ <   `--`./  `--``--`-` `--`./  `--``-.`.____.'                                  > ^ < \r\n /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\  /\\_/\\ \r\n( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )\r\n > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ < ";
+        const string Presentació = "                     Aquest joc ha sigut programat per Sergio Vegas ;)";
+        const string Decoracio = "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\";  
+        const string Explicacio = "Has d'encertar la combinació secreta de 4 numeros diferents, tenint en compte que els numeros dins\nde la combinació estan entre 1 i 6.";
+        const string Explicacio2 = "Has d'inserir un numero i un espai,fins que tinguis una combinació de quatre numeros. Exemple:1 3 4 6";
+        const string SeleccioDificultat = "Selecciona una de les següents dificultats. Has de introduïr el número asignat a la dificultat seleccionada.";
+        const string DificultatNovell = "1. Dificultat novell: Disposarás de 10 intents";
+        const string DificultatAficionat = "2. Dificultat aficionat: Disposarás de 6 intents";
+        const string DificultatExpert = "3. Dificultat expert: Disposarás de 4 intents";
+        const string DificultatMaster = "4. Dificultat Màster: Disposarás de 3 intents";
+        const string DificultatPersonalitzada= "5. Dificultat Personalitzada: Disposarás del intents que tu vulguis. ";
+        const string MsgIntentsDifPersonalitzada = "A continuació has d'introduïr el número d'intents que voldràs.A de ser un nombre entre 1 a 99";
+        const string Go = " ________  ________  ___       \r\n|\\   ____\\|\\   __  \\|\\  \\      \r\n\\ \\  \\___|\\ \\  \\|\\  \\ \\  \\     \r\n \\ \\  \\  __\\ \\  \\\\\\  \\ \\  \\    \r\n  \\ \\  \\|\\  \\ \\  \\\\\\  \\ \\__\\   \r\n   \\ \\_______\\ \\_______\\|__|   \r\n    \\|_______|\\|_______|   ___ \r\n                          |\\__\\\r\n                          \\|__|";
+        const string MsgErrorIntroduccioDades = "Les dades no han sigut introduides correctament";
+        const string NoIntents = $"Ja no tens més intents, gràcies per jugar! La combinació era {CombinacioSecreta}";
+        const string Guanyar = "Felicitats has guanyat!:D";
+        const string ExplicacioPistes = "Durant el lloc rebras unes pistes que t'ayudarán a aconseguir el teu objectiu!";
+        const string ExplicacioPistes2 = "Si surt'O' vol dir que el numero esta en la posició correcta, si surt 'Ø' el numero esta a la combinació\nperò no al lloc i per últim si surt '×' el numero no esta a la combinació.";
+        const string TornarJugar = "Si vols tornar a jugar introdueix el numero 1, si no introdueix un altre numero";
+        const string SortirJoc = "Si vols sortir en qualsevol moment  del joc, introdueix la paraula 'sortir' sense les cometes";
+        const string GraciesJugar = "Moltes gràcies per jugar! :D";
+
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine(MasterMind);
+        Console.WriteLine();
+        Console.WriteLine(Decoracio);
+        Console.WriteLine();
+        Console.WriteLine(Presentació);
+        Console.WriteLine();
+        Console.WriteLine(Decoracio);
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(Explicacio);
+        Console.WriteLine(Explicacio2);
+        
+
+        int Reebot=0;
+        while(Reebot<1)
+        {
+        Console.WriteLine();
+        Console.WriteLine(SeleccioDificultat);
+        Console.WriteLine(DificultatNovell);
+        Console.WriteLine(DificultatAficionat);
+        Console.WriteLine(DificultatExpert);
+        Console.WriteLine(DificultatMaster);
+        Console.WriteLine(DificultatPersonalitzada);
+        int dificultad = 0;
+        try
+        {
+           dificultad = int.Parse(Console.ReadLine());
+        }
+        catch (Exception e) { }
+        
+        int intents = 0;
+
+        switch(dificultad)
+        {
+            case 1:
+                intents = 10;
+                break;
+            case 2: 
+                intents = 6;
+                break;
+            case 3:
+                intents = 4;
+                break;
+            case 4:
+                intents = 3;
+                break;
+            case 5:
+                Console.WriteLine(MsgIntentsDifPersonalitzada);
+                try
+                {
+                intents = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e) { } 
+                break;
+        }
+        if (intents < 1 || intents > 100)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(MsgErrorIntroduccioDades);
+            Console.ForegroundColor = ConsoleColor.White; 
+        }
+        else {
+        Console.WriteLine();        
+        Console.WriteLine(ExplicacioPistes);
+        Console.WriteLine(ExplicacioPistes2);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(Go);
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine();
+            for (int i = 0; i < intents; i++)
+            {
+                int volTornarJugar = 0;
+                string[] arrayCombinacioSecreta = CombinacioSecreta.Split(" ");
+
+                Console.WriteLine($"Ho podrás aconseguir? Et quedan {intents-i} intents!");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(SortirJoc);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                string combinacioUsuari=Console.ReadLine();
+                string[] arrayCombinacioUsuari = combinacioUsuari.Split(" ");
+
+                if (arrayCombinacioUsuari.Length == arrayCombinacioSecreta.Length)
+                {
+                    for (int j = 0; j < arrayCombinacioUsuari.Length; j++)
+                    {
+                        if (arrayCombinacioSecreta[j] == arrayCombinacioUsuari[j])
+                        {
+                        Console.Write("O");
+                        }
+                        else
+                        {
+                            int k = 0;
+                            for (; k < arrayCombinacioSecreta.Length; k++)
+                            {
+                                if (arrayCombinacioUsuari[j] == arrayCombinacioSecreta[k])
+                                {
+                                Console.Write("Ø");
+                                k = arrayCombinacioSecreta.Length + 1;
+                                }
+                            }
+                            if (k == arrayCombinacioSecreta.Length)
+                            {
+                            Console.Write("×");
+                            }
+                        }
+                    }
+                    Console.WriteLine();
+
+                    if (combinacioUsuari == CombinacioSecreta)
+                    {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(Guanyar);
+                    Console.ForegroundColor = ConsoleColor.White; 
+                    i = intents;
+                    Console.WriteLine(TornarJugar);
+                    try
+                    { 
+                    volTornarJugar= int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e) 
+                    { 
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(GraciesJugar);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    if (volTornarJugar != 1)
+                    { Reebot++;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(GraciesJugar);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    }
+                    if (intents == i + 1)
+                    {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(NoIntents);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(TornarJugar);
+                    try
+                    { 
+                    volTornarJugar= int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e) 
+                    { 
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(GraciesJugar);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    if (volTornarJugar != 1)
+                    { Reebot++;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine(GraciesJugar);
+                    Console.ForegroundColor = ConsoleColor.White;}
+                    }
+                }
+                else if(combinacioUsuari=="sortir")
+                {
+                    Reebot++;
+                    i = intents;
+                }
+                else
+                {
+                   Console.ForegroundColor = ConsoleColor.Red;
+                   Console.WriteLine(MsgErrorIntroduccioDades);
+                   Console.ForegroundColor = ConsoleColor.White;
+                if(i+1 == intents) {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine(NoIntents);
+                        Console.ForegroundColor = ConsoleColor.White;       
+                        Console.WriteLine(TornarJugar);
+                        try
+                        { 
+                        volTornarJugar= int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception e) 
+                        { 
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine(GraciesJugar);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        }
+
+                        if (volTornarJugar != 1)
+                        { Reebot++;
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine(GraciesJugar);
+                        Console.ForegroundColor = ConsoleColor.White;    }
+                        }
+                }
+            }
+        }
+        }
+    }
+}
+
